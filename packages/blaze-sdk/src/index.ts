a10e5c129e0da4601774e270690de58a2a098648
Micah Kendall <micahk.accounts@icloud.com>
2024-04-29 20:57:49 +0200

    chore: localised dependencies, top level package, SDK exports all

--- packages/blaze-sdk/src/index.ts
+++ packages/blaze-sdk/src/index.ts
@@ -1,1 +1,7 @@
 export * from "./blaze";
+export * from "@blazecardano/blueprint";
+export * from "@blazecardano/emulator";
+export * from "@blazecardano/query";
+export * from "@blazecardano/tx";
+export * from "@blazecardano/wallet";
+export * as Core from "@blazecardano/core";
