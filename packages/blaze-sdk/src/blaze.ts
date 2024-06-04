e3f468b1a8ae1daac1c03cefc9dc93de288a5343
EzePze <taylorjay098@gmail.com>
2024-06-04 21:11:10 +1000

    Fixed bug in script fee calc, added transaction signing util to SDK

--- packages/blaze-sdk/src/blaze.ts
+++ packages/blaze-sdk/src/blaze.ts
@@ -1,3 +1,5 @@
+import type { Transaction} from "@blaze-cardano/core";
+import { CborSet, VkeyWitness } from "@blaze-cardano/core";
 import type { Provider } from "@blaze-cardano/query";
 import { TxBuilder } from "@blaze-cardano/tx";
 import type { Wallet } from "@blaze-cardano/wallet";
@@ -33,5 +35,33 @@ export class Blaze<ProviderType extends Provider, WalletType extends Wallet> {
       .addUnspentOutputs(myUtxos)
       .setChangeAddress(changeAddress)
       .useEvaluator((x, y) => this.provider.evaluateTransaction(x, y));
+  }
+
+  /**
+   * Signs a transaction using the wallet.
+   * @param {Transaction} tx - The transaction to sign.
+   * @returns {Promise<Transaction>} - The signed transaction.
+   */
+  async signTransaction(tx: Transaction): Promise<Transaction> {
+    const signed = await this.wallet.signTransaction(tx, true);
+    const ws = tx.witnessSet();
+    const vkeys = ws.vkeys()?.toCore() ?? [];
+
+    const signedKeys = signed.vkeys();
+    if (!signedKeys) {
+      throw new Error("signTransaction: no signed keys in wallet witness response");
+    }
+
+    if (
+      signedKeys.toCore().some(([vkey]) => vkeys.some(([key2]) => vkey == key2))
+    ) {
+      throw new Error("signTransaction: some keys were already signed");
+    }
+
+    ws.setVkeys(
+      CborSet.fromCore([...signedKeys.toCore(), ...vkeys], VkeyWitness.fromCore)
+    );
+    tx.setWitnessSet(ws);
+    return tx;
   }
 }
