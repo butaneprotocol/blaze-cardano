f6e3f0ae3633df8a4d52763aa847a2412e591cf8
Martin Schere <tinschere@gmail.com>
2024-11-13 12:46:12 +0100

    chore: fmt

--- packages/blaze-wallet/test/hotsingle.test.ts
+++ packages/blaze-wallet/test/hotsingle.test.ts
@@ -7,14 +7,14 @@ const mockProvider: Provider = jest.fn() as any;
 describe("HotSingleWallet", () => {
   it("constructs correctly with payment and stake key", () => {
     const privateKey = Core.Ed25519PrivateNormalKeyHex(
-      "02f984433fca5ccad486622ebdc5a43c2248fc6305bfec8d67b887dd1802861e"
+      "02f984433fca5ccad486622ebdc5a43c2248fc6305bfec8d67b887dd1802861e",
     );
 
     const wallet = new HotSingleWallet(
       privateKey,
       Core.NetworkId.Mainnet,
       mockProvider,
-      privateKey
+      privateKey,
     );
     console.log(wallet.address.toBech32());
   });
