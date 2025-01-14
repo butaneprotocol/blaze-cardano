fcb1ad1b0f8ae03959ddb39e4e786a8d8256ec34
Pi Lanningham <pi@sundaeswap.finance>
2025-01-14 12:03:42 -0500

    Flesh out the tests a bit more thoroughly
    In reality, I'd love to have a nice 'golden test' suite with real transactions, but couldn't figure out a clean way to do that

--- packages/blaze-tx/tests/tx/method.getScriptData.test.ts
+++ packages/blaze-tx/tests/tx/method.getScriptData.test.ts
@@ -10,6 +10,12 @@ import {
   TransactionUnspentOutput,
   type TransactionWitnessSet,
   type ProtocolParameters,
+  Datum,
+  Script,
+  PlutusV1Script,
+  PlutusV2Script,
+  addressFromValidator,
+  NetworkId,
 } from "@blaze-cardano/core";
 import { TxBuilder } from "../../src";
 import * as value from "../../src/value";
@@ -26,68 +32,75 @@ class TestTxBuilder extends TxBuilder {
 }
 
 describe("TxBuilder.getScriptData", () => {
-  it("should return undefined if no datums or redeemers", async () => {
-    // Real Preview Transaction: ab46256ef7fa8c4aad88be214482c76e1a5b6341c1b27b572d1c01a814a0372d
+  it("should return undefined for no datums and no redeemers", async () => {
     const tx = new TestTxBuilder(hardCodedProtocolParams);
-    tx.addInput(
-      new TransactionUnspentOutput(
-        new TransactionInput(
-          TransactionId(
-            "3228e3b7929f7c6e727f436c2ea2d139fa5dbfdc236739313fc00faed5d52f3c",
-          ),
-          1n,
-        ),
-        new TransactionOutput(
-          Address.fromBech32(
-            "addr_test1qrp8nglm8d8x9w783c5g0qa4spzaft5z5xyx0kp495p8wksjrlfzuz6h4ssxlm78v0utlgrhryvl2gvtgp53a6j9zngqtjfk6s",
-          ),
-          value.makeValue(1_287_000n, [
-            "44a1eb2d9f58add4eb1932bd0048e6a1947e85e3fe4f32956a1104140014df102e74e6af9739616dd021f547bca1f68c937b566bb6ca2e4782e76001",
-            464_705n,
-          ]),
-        ),
-      ),
-    );
 
-    tx.addInput(
-      new TransactionUnspentOutput(
-        new TransactionInput(
-          TransactionId(
-            "3228e3b7929f7c6e727f436c2ea2d139fa5dbfdc236739313fc00faed5d52f3c",
-          ),
-          2n,
-        ),
-        new TransactionOutput(
-          Address.fromBech32(
-            "addr_test1qrp8nglm8d8x9w783c5g0qa4spzaft5z5xyx0kp495p8wksjrlfzuz6h4ssxlm78v0utlgrhryvl2gvtgp53a6j9zngqtjfk6s",
-          ),
-          value.makeValue(2893_406_704n),
-        ),
-      ),
-    );
-
-    tx.lockAssets(
+    tx.payAssets(
       Address.fromBech32(
-        "addr_test1zr866xg5kkvarzll69xjh0tfvqvu9zvuhht2qve9ehmgp0qjrlfzuz6h4ssxlm78v0utlgrhryvl2gvtgp53a6j9zngq4qar8t",
+        "addr_test1qrp8nglm8d8x9w783c5g0qa4spzaft5z5xyx0kp495p8wksjrlfzuz6h4ssxlm78v0utlgrhryvl2gvtgp53a6j9zngqtjfk6s",
       ),
-      value.makeValue(3_280_000n, [
-        "44a1eb2d9f58add4eb1932bd0048e6a1947e85e3fe4f32956a1104140014df102e74e6af9739616dd021f547bca1f68c937b566bb6ca2e4782e76001",
-        116_176n,
-      ]),
-      PlutusData.fromCbor(
-        HexBlob(
-          "d8799fd8799f581c2e74e6af9739616dd021f547bca1f68c937b566bb6ca2e4782e76001ffd8799f581c121fd22e0b57ac206fefc763f8bfa0771919f5218b40691eea4514d0ff1a00138800d8799fd8799fd8799f581cc279a3fb3b4e62bbc78e288783b58045d4ae82a18867d8352d02775affd8799fd8799fd8799f581c121fd22e0b57ac206fefc763f8bfa0771919f5218b40691eea4514d0ffffffffd87980ffd87c9f9f581c44a1eb2d9f58add4eb1932bd0048e6a1947e85e3fe4f32956a11041458200014df102e74e6af9739616dd021f547bca1f68c937b566bb6ca2e4782e760011a0001c5d0ffff43d87980ff",
-        ),
-      ),
+      value.makeValue(1_000_000n),
     );
 
     expect(tx.test__getScriptData()).toBeUndefined();
   });
 
-  it("should correctly build script data when datums are attached but no redeemers", async () => {
+  it("should return undefined for inline datum and no redeemers", async () => {
     const tx = new TestTxBuilder(hardCodedProtocolParams);
 
-    // UTXO #1
+    tx.lockAssets(
+      Address.fromBech32(
+        "addr_test1zr866xg5kkvarzll69xjh0tfvqvu9zvuhht2qve9ehmgp0qjrlfzuz6h4ssxlm78v0utlgrhryvl2gvtgp53a6j9zngq4qar8t",
+      ),
+      value.makeValue(1_000_000n),
+      PlutusData.fromCbor(HexBlob("d87980")),
+    );
+
+    expect(tx.test__getScriptData()).toBeUndefined();
+  });
+
+  it("should return correct script data hash with witness set datums but no redeemers", async () => {
+    const tx = new TestTxBuilder(hardCodedProtocolParams);
+
+    tx.lockAssets(
+      Address.fromBech32(
+        "addr_test1wpesulg5dtt5y73r4zzay9qmy3wnlrxdg944xg4rzuvewls7nrsf0",
+      ),
+      value.makeValue(1_000_000n),
+      DatumHash(
+        "923918e403bf43c34b4ef6b48eb2ee04babed17320d8d1b9ff9ad086e86f44ec",
+      ),
+    );
+
+    tx.provideDatum(PlutusData.fromCbor(HexBlob("d87980")));
+
+    expect(tx.test__getScriptData()).toMatchObject({
+      redeemersEncoded: "a0",
+      datumsEncoded: "d9010281" + "d87980", // nonempty set preamble + datum
+      costModelsEncoded: "a0",
+      hashedData: "a0" + "d9010281" + "d87980a0",
+      scriptDataHash:
+        "264ea21d9904cd72ce5038fa60e0ddd0859383f7fbf60ecec6df22e4c4e34a1f",
+    });
+  });
+
+  it("should return correct script data hash with no datums and a redeemer", async () => {
+    const tx = new TestTxBuilder(hardCodedProtocolParams);
+
+    const alwaysTrueScript: Script = Script.newPlutusV2Script(
+      new PlutusV2Script(HexBlob("510100003222253330044a229309b2b2b9a1")),
+    );
+    const scriptAddress = addressFromValidator(
+      NetworkId.Testnet,
+      alwaysTrueScript,
+    );
+    const output = new TransactionOutput(
+      scriptAddress,
+      value.makeValue(1_000_000n),
+    );
+    output.setDatum(
+      Datum.newInlineData(PlutusData.fromCbor(HexBlob("d87980"))),
+    );
     tx.addInput(
       new TransactionUnspentOutput(
         new TransactionInput(
@@ -96,94 +109,180 @@ describe("TxBuilder.getScriptData", () => {
           ),
           0n,
         ),
-        new TransactionOutput(
-          Address.fromBech32(
-            "addr_test1qrp8nglm8d8x9w783c5g0qa4spzaft5z5xyx0kp495p8wksjrlfzuz6h4ssxlm78v0utlgrhryvl2gvtgp53a6j9zngqtjfk6s",
-          ),
-          value.makeValue(15_000_000n),
-        ),
+        output,
       ),
+      PlutusData.fromCbor(HexBlob("d87980")), // The redeemer to spend the script with
     );
 
-    // UTXO #2
-    tx.addInput(
-      new TransactionUnspentOutput(
-        new TransactionInput(
-          TransactionId(
-            "939b8a271e0e47fd6e8828319bf25cc34564d14d42df6b806b2a3254ea4bf0a4",
-          ),
-          2n,
-        ),
-        new TransactionOutput(
-          Address.fromBech32(
-            "addr_test1qrp8nglm8d8x9w783c5g0qa4spzaft5z5xyx0kp495p8wksjrlfzuz6h4ssxlm78v0utlgrhryvl2gvtgp53a6j9zngqtjfk6s",
-          ),
-          value.makeValue(
-            2_000_000n,
-            [
-              "fa3eff2047fdf9293c5feef4dc85ce58097ea1c6da4845a35153518374494e4459",
-              4_344_137n,
-            ],
-            [
-              "4086577ed57c514f8e29b78f42ef4f379363355a3b65b9a032ee30c96c702002",
-              20n,
-            ],
-          ),
-        ),
-      ),
-    );
+    tx.provideScript(alwaysTrueScript);
 
-    // UTXO #3
-    tx.addInput(
-      new TransactionUnspentOutput(
-        new TransactionInput(
-          TransactionId(
-            "e85c7b3caba4de32ec04de1e8b37d174b06cc921f932daefc05f5b7f08aabf78",
-          ),
-          2n,
-        ),
-        new TransactionOutput(
-          Address.fromBech32(
-            "addr_test1qrp8nglm8d8x9w783c5g0qa4spzaft5z5xyx0kp495p8wksjrlfzuz6h4ssxlm78v0utlgrhryvl2gvtgp53a6j9zngqtjfk6s",
-          ),
-          value.makeValue(2_014_978n, [
-            "4086577ed57c514f8e29b78f42ef4f379363355a3b65b9a032ee30c96c702002",
-            2_165_562n,
-          ]),
-        ),
-      ),
-    );
-
-    tx.lockAssets(
+    tx.payAssets(
       Address.fromBech32(
-        "addr_test1wpesulg5dtt5y73r4zzay9qmy3wnlrxdg944xg4rzuvewls7nrsf0",
+        "addr_test1qrp8nglm8d8x9w783c5g0qa4spzaft5z5xyx0kp495p8wksjrlfzuz6h4ssxlm78v0utlgrhryvl2gvtgp53a6j9zngqtjfk6s",
       ),
-      value.makeValue(4_500_000n, [
-        "4086577ed57c514f8e29b78f42ef4f379363355a3b65b9a032ee30c96c702002",
-        6_509_699n,
-      ]),
-      DatumHash(
-        "ee5efdd83746ef1067de71e8ffa7cefeba312b4592531b193aa9d6d47ba1aad1",
-      ),
+      value.makeValue(1_000_000n),
     );
 
-    tx.provideDatum(
-      PlutusData.fromCbor(
-        HexBlob(
-          "d8799f4102d8799fd8799fd8799fd8799f581cc279a3fb3b4e62bbc78e288783b58045d4ae82a18867d8352d02775affd8799fd8799fd8799f581c121fd22e0b57ac206fefc763f8bfa0771919f5218b40691eea4514d0ffffffffd87a80ffd87a80ff1a002625a0d87a9f1a00635483ffff",
-        ),
-      ),
-    );
-
+    const plutusV2CostModels =
+      "a10198af1a000189b41901a401011903e818ad00011903e819ea350401192baf18201a000312591920a404193e801864193e801864193e801864193e801864193e801864193e80186418641864193e8018641a000170a718201a00020782182019f016041a0001194a18b2000119568718201a0001643519030104021a00014f581a00037c71187a0001011903e819a7a90402195fe419733a1826011a000db464196a8f0119ca3f19022e011999101903e819ecb2011a00022a4718201a000144ce1820193bc318201a0001291101193371041956540a197147184a01197147184a0119a9151902280119aecd19021d0119843c18201a00010a9618201a00011aaa1820191c4b1820191cdf1820192d1a18201a00014f581a00037c71187a0001011a0001614219020700011a000122c118201a00014f581a00037c71187a0001011a00014f581a00037c71187a0001011a000e94721a0003414000021a0004213c19583c041a00163cad19fc3604194ff30104001a00022aa818201a000189b41901a401011a00013eff182019e86a1820194eae182019600c1820195108182019654d182019602f18201a0290f1e70a1a032e93af1937fd0a1a0298e40b1966c40a";
     expect(tx.test__getScriptData()).toMatchObject({
-      redeemersEncoded: "a0",
-      datumsEncoded:
-        "d9010281d8799f4102d8799fd8799fd8799fd8799f581cc279a3fb3b4e62bbc78e288783b58045d4ae82a18867d8352d02775affd8799fd8799fd8799f581c121fd22e0b57ac206fefc763f8bfa0771919f5218b40691eea4514d0ffffffffd87a80ffd87a80ff1a002625a0d87a9f1a00635483ffff",
-      costModelsEncoded: "a0",
+      redeemersEncoded: "a182000082d87980821a00d59f801b00000002540be400",
+      datumsEncoded: undefined,
+      costModelsEncoded: plutusV2CostModels,
       hashedData:
-        "a0d9010281d8799f4102d8799fd8799fd8799fd8799f581cc279a3fb3b4e62bbc78e288783b58045d4ae82a18867d8352d02775affd8799fd8799fd8799f581c121fd22e0b57ac206fefc763f8bfa0771919f5218b40691eea4514d0ffffffffd87a80ffd87a80ff1a002625a0d87a9f1a00635483ffffa0",
+        "a182000082d87980821a00d59f801b00000002540be400a10198af1a000189b41901a401011903e818ad00011903e819ea350401192baf18201a000312591920a404193e801864193e801864193e801864193e801864193e801864193e80186418641864193e8018641a000170a718201a00020782182019f016041a0001194a18b2000119568718201a0001643519030104021a00014f581a00037c71187a0001011903e819a7a90402195fe419733a1826011a000db464196a8f0119ca3f19022e011999101903e819ecb2011a00022a4718201a000144ce1820193bc318201a0001291101193371041956540a197147184a01197147184a0119a9151902280119aecd19021d0119843c18201a00010a9618201a00011aaa1820191c4b1820191cdf1820192d1a18201a00014f581a00037c71187a0001011a0001614219020700011a000122c118201a00014f581a00037c71187a0001011a00014f581a00037c71187a0001011a000e94721a0003414000021a0004213c19583c041a00163cad19fc3604194ff30104001a00022aa818201a000189b41901a401011a00013eff182019e86a1820194eae182019600c1820195108182019654d182019602f18201a0290f1e70a1a032e93af1937fd0a1a0298e40b1966c40a",
       scriptDataHash:
-        "c890ff82c5188d645850890263c1519b0728b6b3e633a1d6e8a30bfe34f8ce31",
+        "710814372ce5f98af866f23e79f6bc403805f0bb9be795e0ac3d930b7a8f627c",
     });
   });
+
+  it("should return correct script data hash with datums and redeemers", async () => {
+    const tx = new TestTxBuilder(hardCodedProtocolParams);
+
+    const alwaysTrueScript: Script = Script.newPlutusV2Script(
+      new PlutusV2Script(HexBlob("510100003222253330044a229309b2b2b9a1")),
+    );
+    const scriptAddress = addressFromValidator(
+      NetworkId.Testnet,
+      alwaysTrueScript,
+    );
+    const output = new TransactionOutput(
+      scriptAddress,
+      value.makeValue(1_000_000n),
+    );
+    output.setDatum(
+      Datum.newDataHash(
+        DatumHash(
+          "092e6ef8c4c0ead37a7a1774a7490ca76dd83df3ee22b64244668cef6eb20905",
+        ),
+      ),
+    );
+    tx.addInput(
+      new TransactionUnspentOutput(
+        new TransactionInput(
+          TransactionId(
+            "a7d0a23abf5f0389a2b17df1125539a0e43ee8b1f13c56d6ba406aa557ade1d2",
+          ),
+          0n,
+        ),
+        output,
+      ),
+      PlutusData.fromCbor(HexBlob("d87980")), // The redeemer to spend the script with,
+      PlutusData.fromCbor(HexBlob("d87980")), // Datum
+    );
+
+    tx.provideScript(alwaysTrueScript);
+
+    tx.payAssets(
+      Address.fromBech32(
+        "addr_test1qrp8nglm8d8x9w783c5g0qa4spzaft5z5xyx0kp495p8wksjrlfzuz6h4ssxlm78v0utlgrhryvl2gvtgp53a6j9zngqtjfk6s",
+      ),
+      value.makeValue(1_000_000n),
+    );
+
+    const plutusV2CostModels =
+      "a10198af1a000189b41901a401011903e818ad00011903e819ea350401192baf18201a000312591920a404193e801864193e801864193e801864193e801864193e801864193e80186418641864193e8018641a000170a718201a00020782182019f016041a0001194a18b2000119568718201a0001643519030104021a00014f581a00037c71187a0001011903e819a7a90402195fe419733a1826011a000db464196a8f0119ca3f19022e011999101903e819ecb2011a00022a4718201a000144ce1820193bc318201a0001291101193371041956540a197147184a01197147184a0119a9151902280119aecd19021d0119843c18201a00010a9618201a00011aaa1820191c4b1820191cdf1820192d1a18201a00014f581a00037c71187a0001011a0001614219020700011a000122c118201a00014f581a00037c71187a0001011a00014f581a00037c71187a0001011a000e94721a0003414000021a0004213c19583c041a00163cad19fc3604194ff30104001a00022aa818201a000189b41901a401011a00013eff182019e86a1820194eae182019600c1820195108182019654d182019602f18201a0290f1e70a1a032e93af1937fd0a1a0298e40b1966c40a";
+    expect(tx.test__getScriptData()).toMatchObject({
+      redeemersEncoded: "a182000082d87980821a00d59f801b00000002540be400",
+      datumsEncoded: "d9010281" + "d87980",
+      costModelsEncoded: plutusV2CostModels,
+      hashedData:
+        "a182000082d87980821a00d59f801b00000002540be400d9010281d87980a10198af1a000189b41901a401011903e818ad00011903e819ea350401192baf18201a000312591920a404193e801864193e801864193e801864193e801864193e801864193e80186418641864193e8018641a000170a718201a00020782182019f016041a0001194a18b2000119568718201a0001643519030104021a00014f581a00037c71187a0001011903e819a7a90402195fe419733a1826011a000db464196a8f0119ca3f19022e011999101903e819ecb2011a00022a4718201a000144ce1820193bc318201a0001291101193371041956540a197147184a01197147184a0119a9151902280119aecd19021d0119843c18201a00010a9618201a00011aaa1820191c4b1820191cdf1820192d1a18201a00014f581a00037c71187a0001011a0001614219020700011a000122c118201a00014f581a00037c71187a0001011a00014f581a00037c71187a0001011a000e94721a0003414000021a0004213c19583c041a00163cad19fc3604194ff30104001a00022aa818201a000189b41901a401011a00013eff182019e86a1820194eae182019600c1820195108182019654d182019602f18201a0290f1e70a1a032e93af1937fd0a1a0298e40b1966c40a",
+      scriptDataHash:
+        "e1b227554d79136615d527db574e58c432326010022d9e89f0eb58d743d8470d",
+    });
+  });
+
+  it("should return correct script data hash with multiple script versions", async () => {
+    const tx = new TestTxBuilder(hardCodedProtocolParams);
+
+    const alwaysTrueV1: Script = Script.newPlutusV1Script(
+      new PlutusV1Script(HexBlob("510100003222253330044a229309b2b2b9a1")),
+    );
+    const alwaysTrueV2: Script = Script.newPlutusV2Script(
+      new PlutusV2Script(HexBlob("510100003222253330044a229309b2b2b9a1")),
+    );
+    const scriptAddressV1 = addressFromValidator(
+      NetworkId.Testnet,
+      alwaysTrueV1,
+    );
+    const scriptAddressV2 = addressFromValidator(
+      NetworkId.Testnet,
+      alwaysTrueV2,
+    );
+    const outputV1 = new TransactionOutput(
+      scriptAddressV1,
+      value.makeValue(1_000_000n),
+    );
+    const outputV2 = new TransactionOutput(
+      scriptAddressV2,
+      value.makeValue(1_000_000n),
+    );
+    outputV1.setDatum(
+      Datum.newDataHash(
+        DatumHash(
+          "092e6ef8c4c0ead37a7a1774a7490ca76dd83df3ee22b64244668cef6eb20905",
+        ),
+      ),
+    );
+    outputV2.setDatum(
+      Datum.newDataHash(
+        DatumHash(
+          "8392f0c940435c06888f9bdb8c74a95dc69f156367d6a089cf008ae05caae01e",
+        ),
+      ),
+    );
+    tx.addInput(
+      new TransactionUnspentOutput(
+        new TransactionInput(
+          TransactionId(
+            "a7d0a23abf5f0389a2b17df1125539a0e43ee8b1f13c56d6ba406aa557ade1d2",
+          ),
+          0n,
+        ),
+        outputV1,
+      ),
+      PlutusData.fromCbor(HexBlob("d87980")), // The redeemer to spend the script with,
+      PlutusData.fromCbor(HexBlob("d87980")), // Datum
+    );
+    tx.addInput(
+      new TransactionUnspentOutput(
+        new TransactionInput(
+          TransactionId(
+            "a7d0a23abf5f0389a2b17df1125539a0e43ee8b1f13c56d6ba406aa557ade1d2",
+          ),
+          1n,
+        ),
+        outputV2,
+      ),
+      PlutusData.fromCbor(HexBlob("d87a80")), // The redeemer to spend the script with,
+      PlutusData.fromCbor(HexBlob("d87a80")), // Datum
+    );
+
+    tx.provideScript(alwaysTrueV1);
+    tx.provideScript(alwaysTrueV2);
+
+    tx.payAssets(
+      Address.fromBech32(
+        "addr_test1qrp8nglm8d8x9w783c5g0qa4spzaft5z5xyx0kp495p8wksjrlfzuz6h4ssxlm78v0utlgrhryvl2gvtgp53a6j9zngqtjfk6s",
+      ),
+      value.makeValue(1_000_000n),
+    );
+
+    const redeemersEncoded =
+      "a282000082d87980821a00d59f801b00000002540be40082000182d87a80821a00d59f801b00000002540be400";
+    const datumsEncoded = "d9010282" + "d87980" + "d87a80";
+    const costModelsEncoded =
+      "a20198af1a000189b41901a401011903e818ad00011903e819ea350401192baf18201a000312591920a404193e801864193e801864193e801864193e801864193e801864193e80186418641864193e8018641a000170a718201a00020782182019f016041a0001194a18b2000119568718201a0001643519030104021a00014f581a00037c71187a0001011903e819a7a90402195fe419733a1826011a000db464196a8f0119ca3f19022e011999101903e819ecb2011a00022a4718201a000144ce1820193bc318201a0001291101193371041956540a197147184a01197147184a0119a9151902280119aecd19021d0119843c18201a00010a9618201a00011aaa1820191c4b1820191cdf1820192d1a18201a00014f581a00037c71187a0001011a0001614219020700011a000122c118201a00014f581a00037c71187a0001011a00014f581a00037c71187a0001011a000e94721a0003414000021a0004213c19583c041a00163cad19fc3604194ff30104001a00022aa818201a000189b41901a401011a00013eff182019e86a1820194eae182019600c1820195108182019654d182019602f18201a0290f1e70a1a032e93af1937fd0a1a0298e40b1966c40a41005901a69f1a000189b41901a401011903e818ad00011903e819ea350401192baf18201a000312591920a404193e801864193e801864193e801864193e801864193e801864193e80186418641864193e8018641a000170a718201a00020782182019f016041a0001194a18b2000119568718201a0001643519030104021a00014f581a00037c71187a0001011903e819a7a90402195fe419733a1826011a000db464196a8f0119ca3f19022e011999101903e819ecb2011a00022a4718201a000144ce1820193bc318201a0001291101193371041956540a197147184a01197147184a0119a9151902280119aecd19021d0119843c18201a00010a9618201a00011aaa1820191c4b1820191cdf1820192d1a18201a00014f581a00037c71187a0001011a0001614219020700011a000122c118201a00014f581a00037c71187a0001011a00014f581a00037c71187a0001011a0004213c19583c041a00163cad19fc3604194ff30104001a00022aa818201a000189b41901a401011a00013eff182019e86a1820194eae182019600c1820195108182019654d182019602f18201a032e93af1937fd0aff";
+    expect(tx.test__getScriptData()).toMatchObject({
+      redeemersEncoded,
+      datumsEncoded,
+      costModelsEncoded,
+      hashedData: redeemersEncoded + datumsEncoded + costModelsEncoded,
+      scriptDataHash:
+        "180262ca6cf7b0de40f7063cefbc23a1f931872af77f3f3e9df2ab05668de112",
+    });
+  });
+
+  // TODO: add some golden tests based on some raw script bytes
 });
