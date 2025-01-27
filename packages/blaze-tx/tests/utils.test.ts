593dc583bc9e607e9d483d028ddd97b0eebbbbd1
Calvin Koepke <hello@calvinkoepke.com>
2025-01-27 10:38:22 -0700

    chore: more tests

--- packages/blaze-tx/tests/utils.test.ts
+++ packages/blaze-tx/tests/utils.test.ts
@@ -1,10 +1,13 @@
 import {
+  Address,
   hardCodedProtocolParams,
   HexBlob,
   PlutusV3Script,
   Script,
+  TransactionOutput,
 } from "@blaze-cardano/core";
-import { calculateReferenceScriptFee } from "../src";
+import * as value from "../src/value";
+import { calculateMinAda, calculateReferenceScriptFee } from "../src";
 
 describe("Fee Calculation", () => {
   it("should calculate the correct min fee for reference inputs", () => {
@@ -25,4 +28,26 @@ describe("Fee Calculation", () => {
       ),
     ).toBe(4489380);
   });
+
+  it("should calculate the correct minADA when a different coinsPerUtxoByte param is provided", () => {
+    const output = new TransactionOutput(
+      Address.fromBech32(
+        "addr_test1qrp8nglm8d8x9w783c5g0qa4spzaft5z5xyx0kp495p8wksjrlfzuz6h4ssxlm78v0utlgrhryvl2gvtgp53a6j9zngqtjfk6s",
+      ),
+      value.makeValue(100_000n)
+    )
+
+    expect(
+      calculateMinAda(
+        output,
+      ).toString()
+    ).toEqual("969750")
+
+    expect(
+      calculateMinAda(
+        output,
+        5000
+      ).toString()
+    ).toEqual("1125000")
+  })
 });
