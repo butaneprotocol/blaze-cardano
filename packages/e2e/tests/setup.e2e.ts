81179e4812313ea7a2781a865bdba955e9d1e523
Micah Kendall <micahk.accounts@icloud.com>
2025-08-11 14:15:58 +1000

    fmt

--- packages/e2e/tests/setup.e2e.ts
+++ packages/e2e/tests/setup.e2e.ts
@@ -1,6 +1,6 @@
 export {};
-declare const jest: { retryTimes?: (n: number) => void; setTimeout?: (ms: number) => void } | undefined;
+declare const jest:
+  | { retryTimes?: (n: number) => void; setTimeout?: (ms: number) => void }
+  | undefined;
 jest?.retryTimes?.(2);
 jest?.setTimeout?.(120_000);
-
-
