9cdecfdba712fed17074f4406509df8e8dc85250
Micah Kendall <micahk.accounts@icloud.com>
2025-08-11 11:05:42 +1000

    more methods. (todo: finish params,add e2e tests)

--- packages/blaze-core/src/types.ts
+++ packages/blaze-core/src/types.ts
@@ -178,6 +178,12 @@ export type UpdateDelegateRepresentative =
 export const Anchor = C.Serialization.Anchor;
 export type Anchor = C.Serialization.Anchor;
 
+// Governance and DRep related
+export const DRep = C.Serialization.DRep;
+export type DRep = C.Serialization.DRep;
+export type GovernanceAction = C.Cardano.GovernanceAction;
+export type DelegateRepresentative = C.Cardano.DelegateRepresentative;
+
 export const VotingProcedures = C.Serialization.VotingProcedures;
 export type VotingProcedures = C.Serialization.VotingProcedures;
 
@@ -186,6 +192,27 @@ export type VotingProcedure = C.Serialization.VotingProcedure;
 
 export const Voter = C.Serialization.Voter;
 export type Voter = C.Serialization.Voter;
+
+// Additional governance-related types
+export const VoteDelegation = C.Serialization.VoteDelegation;
+export type VoteDelegation = C.Serialization.VoteDelegation;
+
+export const ProposalProcedure = C.Serialization.ProposalProcedure;
+export type ProposalProcedure = C.Serialization.ProposalProcedure;
+
+export const GovernanceActionId = C.Serialization.GovernanceActionId;
+export type GovernanceActionId = C.Serialization.GovernanceActionId;
+
+// Stake Pool
+export type PoolParameters = C.Cardano.PoolParameters;
+export type EpochNo = C.Cardano.EpochNo;
+export const PoolRegistration = C.Serialization.PoolRegistration;
+export type PoolRegistration = C.Serialization.PoolRegistration;
+export const PoolRetirement = C.Serialization.PoolRetirement;
+export type PoolRetirement = C.Serialization.PoolRetirement;
+
+// Anchor core type
+export type AnchorCore = C.Cardano.Anchor;
 
 export const Ed25519SignatureHex = Crypto.Ed25519SignatureHex;
 export type Ed25519SignatureHex = Crypto.Ed25519SignatureHex;
