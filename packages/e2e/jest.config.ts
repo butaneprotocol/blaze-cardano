4b00a86811ccf100ddd956f20d66a2af9ea5d15d
Micah Kendall <micahk.accounts@icloud.com>
2025-08-11 14:00:07 +1000

    E2e structure + CI/CD

---
+++ packages/e2e/jest.config.ts
@@ -0,0 +1,15 @@
+import base from '@blaze-cardano/jest-config/base.jest.config';
+import { config as dotenv } from 'dotenv';
+
+dotenv();
+
+export default {
+  ...base,
+  testMatch: ['**/tests/**/*.e2e.test.ts'],
+  testTimeout: 120000,
+  maxWorkers: 1,
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.e2e.ts'],
+  testEnvironment: 'node'
+};
+
+
---
+++ packages/e2e/tests/vacuous.e2e.test.ts
@@ -0,0 +1,7 @@
+describe('vacuous', () => {
+  it('test', () => {
+    expect(true).toBe(true);
+  });
+});
+
+
