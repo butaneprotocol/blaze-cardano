05b1f20980530358a04ebfd06ccff418d901115c
Micah Kendall <micahk.accounts@icloud.com>
2025-08-11 14:15:58 +1000

    fmt

--- packages/e2e/jest.config.ts
+++ packages/e2e/jest.config.ts
@@ -1,15 +1,13 @@
-import base from '@blaze-cardano/jest-config/base.jest.config';
-import { config as dotenv } from 'dotenv';
+import base from "@blaze-cardano/jest-config/base.jest.config";
+import { config as dotenv } from "dotenv";
 
 dotenv();
 
 export default {
   ...base,
-  testMatch: ['**/tests/**/*.e2e.test.ts'],
+  testMatch: ["**/tests/**/*.e2e.test.ts"],
   testTimeout: 120000,
   maxWorkers: 1,
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.e2e.ts'],
-  testEnvironment: 'node'
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.e2e.ts"],
+  testEnvironment: "node",
 };
-
-
--- packages/e2e/tests/example.e2e.test.ts
+++ packages/e2e/tests/example.e2e.test.ts
@@ -1,11 +1,9 @@
-import { TxBuilder } from '@blaze-cardano/tx';
-import { hardCodedProtocolParams } from '@blaze-cardano/core';
+import { TxBuilder } from "@blaze-cardano/tx";
+import { hardCodedProtocolParams } from "@blaze-cardano/core";
 
-describe('e2e wiring', () => {
-  it('constructs a TxBuilder with protocol params', () => {
+describe("e2e wiring", () => {
+  it("constructs a TxBuilder with protocol params", () => {
     const txb = new TxBuilder(hardCodedProtocolParams);
     expect(txb).toBeTruthy();
   });
 });
-
-
