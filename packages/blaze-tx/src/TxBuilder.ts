3ef18b9af9c0175e1aa1f196517e0d56c2883b2b
EzePze <taylorjay098@gmail.com>
2025-09-15 17:27:29 +1000

    wip: need to consider drep and spo holdings not just delegated stake

--- packages/blaze-tx/src/TxBuilder.ts
+++ packages/blaze-tx/src/TxBuilder.ts
@@ -1431,6 +1431,8 @@ export class TxBuilder {
       this.params.minFeeConstant + txSize * this.params.minFeeCoefficient,
     );
 
+    console.log("txSize", txSize)
+
     let refScriptFee = 0;
     if (this.params.minFeeReferenceScripts) {
       // The minFeeReferenceScripts parameter governs the cost of *reference scripts*, not to be confused with *reference inputs*
@@ -2168,6 +2170,7 @@ export class TxBuilder {
    */
   setVotingProcedures(votingProcedures: VotingProcedures): TxBuilder {
     this.body.setVotingProcedures(votingProcedures);
+    console.log(this.body.votingProcedures()!.toCore())
     return this;
   }
 
