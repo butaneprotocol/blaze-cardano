27261a6e8d6d62461903f2ade27aaa97ffd1a3c4
Simon Gellis <simongellis@gmail.com>
2026-01-22 15:24:38 -0500

    docs: add doc comment on initCrypto method

--- packages/blaze-core/src/init.ts
+++ packages/blaze-core/src/init.ts
@@ -1,4 +1,9 @@
 import * as Crypto from "@cardano-sdk/crypto";
+/**
+ * Initialize the `@cardano-sdk/crypto` module.
+ * This function must be called before calling any exported functions from `@cardano-sdk/crypto`. It is safe to call multiple times.
+ * Blaze should be calling this internally before using any exports from that module. If it does not, please file a bug.
+ */
 export async function initCrypto() {
   await Crypto.ready();
 }
